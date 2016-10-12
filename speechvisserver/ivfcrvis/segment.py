6ed84987d1a1e12abc620c67053eda090a79eda9
Tim Shea <tim.m.shea@gmail.com>
2016-10-12 15:01:34 -0700

    Next

--- speechvisserver/ivfcrvis/segment.py
+++ speechvisserver/ivfcrvis/segment.py
@@ -6,8 +6,8 @@ import numpy
 def plot_segment(segment, window_size, step_size):
     """Plots the waveform, power over time, spectrogram with formants, and power over frequency of a Segment."""
     fig = pyplot.figure()
+    pyplot.suptitle('Vocalization {}'.format(segment.number))
     pyplot.subplot(2, 2, 1)
-    segment.read_audio()
     pyplot.plot(numpy.linspace(0, float(segment.duration), len(segment.signal)), segment.signal)
     pyplot.xlim(0, segment.duration)
     pyplot.xlabel('Time (s)')
@@ -20,8 +20,8 @@ def plot_segment(segment, window_size, step_size):
     pyplot.ylabel('Power (dB)')
     pyplot.subplot(2, 2, 3)
     pyplot.specgram(segment.signal, NFFT=window_size, Fs=segment.samplerate, noverlap=window_size - step_size)
-    formants = segment.formants(window_size, step_size, 4)
-    pyplot.plot(numpy.linspace(0, segment.duration, len(formants)), formants, 'o')
+    windows, pitches = segment.pitch(window_size, step_size)
+    pyplot.plot(numpy.array(windows) / segment.samplerate, pitches, 'or')
     pyplot.xlim(0, segment.duration)
     pyplot.xlabel('Time (s)')
     pyplot.ylabel('Frequency (Hz)')
--- speechvisserver/models.py
+++ speechvisserver/models.py
@@ -3,7 +3,7 @@ from django.db.models import Max
 from xml.etree import ElementTree
 import numpy
 from scipy.io import wavfile
-from scipy.signal import butter, lfilter, correlate
+from scipy.signal import butter, lfilter, correlate, savgol_filter
 from python_speech_features import logfbank, mfcc
 import os.path
 
@@ -140,6 +140,13 @@ def parse_category(segment):
 
 
 class Segment(models.Model):
+    @staticmethod
+    def find_peak(signal):
+        diff = numpy.diff(signal)
+        extrema = numpy.where(diff[:-1] * diff[1:] <= 0)[0]
+        index = numpy.arange(len(signal))
+        return extrema[numpy.argmax(signal[extrema])]
+
     recording = models.ForeignKey(Recording, on_delete=models.CASCADE, related_name='segment')
     number = models.IntegerField()
     start = models.FloatField()
@@ -168,11 +175,11 @@ class Segment(models.Model):
         window_size = 1024
         step_size = 512
         _, power = self.power(window_size, step_size)
-        pitch = self.pitch(window_size, step_size)
+        _, pitches = self.pitch(window_size, step_size)
         return {
             'peak_amplitude': power.max(),
             'mean_amplitude': power.mean(),
-            'pitch': pitch
+            'pitch': numpy.mean(pitches)
         }
 
     def power(self, window_size, step_size):
@@ -186,23 +193,47 @@ class Segment(models.Model):
             power[i] = 10 * numpy.log10(rms)
         return steps / float(self.samplerate), power
 
-    def pitch(self, window_size, step_size):
-        nyquist = 0.5 * self.samplerate
-        low = 50 / nyquist
-        high = 600 / nyquist
-        order = 5
+    def filter(self, lowFreq, highFreq, order):
+        nyquist = 0.5 * float(self.samplerate)
+        low = lowFreq / nyquist
+        high = highFreq / nyquist
         b, a = butter(order, [low, high], btype='band')
+        self.signal = lfilter(b, a, self.signal)
+
+    def pitch(self, window_size, step_size, lowCut=50, highCut=600, threshold=40):
         steps = numpy.arange(0, self.signal.size - window_size + 1, step_size)
-        pitch = numpy.zeros(len(steps))
+        windows = []
+        pitches = []
         for i, step in zip(range(len(steps)), steps):
-            windowed_signal = self.signal[step:step + window_size].astype(int)
+            windowed_signal = self.signal[step:step + window_size]
+            nyquist = 0.5 * float(self.samplerate)
+            low = lowCut / nyquist
+            high = highCut / nyquist
+            b, a = butter(4, [low, high], btype='band')
             filtered_signal = lfilter(b, a, windowed_signal)
-            spectrum = abs(numpy.fft.rfft(filtered_signal).real)
-            cepstrum = abs(numpy.fft.rfft(spectrum).real)
-            for j in range(5, len(cepstrum) - 5):
-                if cepstrum[j] >= cepstrum[j - 5:j + 5].max():
-                    pitch[i] = j
-        return self.samplerate / pitch.mean()
+            pitch = self.autocorr_pitch(filtered_signal, lowCut, highCut)
+            frequencies = numpy.fft.rfftfreq(len(filtered_signal), 1 / self.samplerate)
+            spectrum = 10 * numpy.log10(numpy.abs(numpy.fft.rfft(windowed_signal).real))
+            f = numpy.argmin(numpy.abs(frequencies - pitch))
+            power = spectrum[f - 2:f + 3].mean()
+            if power >= threshold:
+                windows.append(step + window_size / 2)
+                pitches.append(pitch)
+        return windows, pitches
+
+    def autocorr_pitch(self, signal, low, high):
+        autocorr = correlate(signal, signal)
+        autocorr = autocorr[len(autocorr) / 2:]
+        autocorr = autocorr[int(self.samplerate / high):int(self.samplerate / low)]
+        return self.samplerate / (Segment.find_peak(autocorr) + int(self.samplerate / high))
+
+    def cepstral_pitch(self, signal):
+        spectrum = numpy.fft.rfft(signal).real
+        cepstrum = numpy.fft.rfft(numpy.log(numpy.square(spectrum) + 0.0001)).real
+        for peak in range(len(cepstrum) - 2, 1, -1):
+            power = cepstrum[peak]
+            if power > cepstrum[peak - 1] and power > cepstrum[peak + 1]:
+                return peak, power, cepstrum
 
     def power_spectrum(self, window_size, step_size):
         """Return a time series of power spectra, where each spectrum corresponds to the acoustic power within a window
